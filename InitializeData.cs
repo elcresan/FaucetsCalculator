cd935e35b630576e8367339bdd9a924b1e6ca25a
Silvia Carrasco <sil4518@gmail.com>
2017-12-22 21:42:39 +0100

    Add miners.

--- InitializeData.cs
+++ InitializeData.cs
@@ -6,8 +6,14 @@ namespace FaucetsCalculator {
         public static List<Miner> GetMiners(){
             List<Miner> miners = new List<Miner>();
 
-            miners.Add(new Miner(cost: 30, generates: 30));
-            // TODO: Add the rest of miners;
+            miners.Add(new Miner(cost: 30, generates: 30, name: "First"));
+            miners.Add(new Miner(cost: 200, generates: 220, name: "Second"));
+            miners.Add(new Miner(cost: 1500, generates: 1800, name: "Third"));
+            miners.Add(new Miner(cost: 5000, generates: 7000, name: "Fourth"));
+            miners.Add(new Miner(cost: 25000, generates: 37000, name: "Fifth"));
+            miners.Add(new Miner(cost: 100000, generates: 150000, name: "Sixth"));
+            miners.Add(new Miner(cost: 250000, generates: 400000, name: "Seventh"));
+            miners.Add(new Miner(cost: 500000, generates: 850000, name: "Eighth"));
 
             return miners;
         }
--- Miner.cs
+++ Miner.cs
@@ -2,16 +2,20 @@ namespace FaucetsCalculator
 {
     internal class Miner {
 
-        public Miner(int cost, int generates)
+        public Miner(int cost, int generates, string name)
         {
             this.Cost = cost;
             this.Generates = generates;
+            this.Name = name;
         }
 
         /// <summary> The cost of buying the miner. </summary>
         int Cost { get; set; }
-       
+
         /// <summary> The money that the miner generates each period. </summary>
-         int Generates { get; set; }
+        int Generates { get; set; }
+
+        /// <summary> The name of the miner. </summary>
+        string Name { get; set; }
     }
 }
